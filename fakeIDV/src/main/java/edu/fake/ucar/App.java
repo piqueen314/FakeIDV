0152a982a7a5d30827df8f56bd38f93f82d20587
Cece Hedrick <cecehedrick@gmail.com>
2018-05-01 15:34:13 -0600

    removes submodule class preparing for git subtree

--- fakeIDV/src/main/java/edu/fake/ucar/App.java
+++ fakeIDV/src/main/java/edu/fake/ucar/App.java
@@ -1,6 +1,6 @@
 package edu.fake.ucar;
 import edu.fake.ucar.fakeIDVPackage.FakeIDVClass;
-import edu.fake.ssec.FakeHydraGitSubModule.fakeHydra.FakeHydraClass1;
+
 
 import java.util.Arrays;
 
@@ -18,11 +18,7 @@ public class App
         System.out.println(IDVClassName);
 
 
-        // Yay! Now I can use the fakeHydra package :)
-        FakeHydraClass1 fakeHydraClass1 = new FakeHydraClass1();
-        double [][] data = {{10.0, 3.0, 7.0, 6.0}, {1.0, 12.0, 15.0, 2.0}, {3.0, 9.0, 11.0, 7.0}};
-        double [] columnSums = fakeHydraClass1.columnSums(data);
-        String colSimStrings = Arrays.toString(columnSums);
-        System.out.print(colSimStrings);
+        // sure wish I could use the fakeHydra package :(
+
     }
 }
